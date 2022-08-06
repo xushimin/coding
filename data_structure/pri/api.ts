1eae2369c3912e99a60e966a1b62eb01b242a912
xushimin <xushimin@bytedance.com>
2022-08-06 20:51:48 +0800

    two test

--- data_structure/pri/api.ts
+++ data_structure/pri/api.ts
@@ -1,4 +1,9 @@
 class MaxPQ {
+  static main(size){
+    const M = size;
+
+  }
+  // 可以指定大小，还可以用于定的一个数组将其初始化
   constructor(max: number) {
 
   }
@@ -21,6 +26,6 @@ class MaxPQ {
 
 
   less(key1, key2) {
-
+    return this.max.compareTo(key1, key2);
   }
 }
